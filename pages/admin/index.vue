03f4d932be3851af3b84cf73222041cf2e9311a2
jon <13560586496@163.com>
2019-09-23 21:52:36 +0800

    feat: add browser pie

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,10 +1,15 @@
 <template>
 	<section class="admin_data">
 		<div class="card_list">
-			<div class="PV_data card col7">
-				<!-- <canvas>你的浏览器不支持canvas,请升级你的浏览器</canvas> -->
+			<div class="PV_data_wrap card col7">
+				<div class="PV_data"></div>
 			</div>
-			<div class="browser_data card col3"></div>
+			<div class="browser_data_wrap card col3">
+				<div class="browser_data"></div>
+			</div>
+			<div class="map_data_wrap card">
+				<div class="map_data"></div>
+			</div>
 		</div>
 	</section>
 </template>
@@ -13,7 +18,9 @@
 // import echarts from "echarts";
 import echarts from "echarts/lib/echarts";
 import line from "echarts/lib/chart/line";
+import pie from "echarts/lib/chart/pie";
 import tooltip from "echarts/lib/component/tooltip";
+import legend from "echarts/lib/component/legend";
 
 export default {
 	layout: "admin",
@@ -22,13 +29,12 @@ export default {
 	},
 	mounted() {
 		this.PV();
+		this.browser();
+		this.map();
 	},
 	methods: {
 		PV() {
-			document.getElementsByClassName("PV_data")[0].style.width = "70%";
-			document.getElementsByClassName("PV_data")[0].style.height =
-				"500px";
-			var myPv = echarts.init(
+			let myPv = echarts.init(
 				document.getElementsByClassName("PV_data")[0]
 			);
 			myPv.setOption({
@@ -63,7 +69,46 @@ export default {
 					}
 				}
 			});
-		}
+		},
+		browser() {
+			let myBrowser = echarts.init(
+				document.getElementsByClassName("browser_data")[0]
+			);
+			myBrowser.setOption({
+				tooltip: {
+					trigger: "item",
+					formatter: "{a} <br/>{b} : {c} ({d}%)"
+				},
+				legend: {
+					orient: "vertical",
+					left: "left",
+					data: ["Chrome", "Safari", "Opera", "Firefox", "IE"]
+				},
+				series: [
+					{
+						name: "访问来源",
+						type: "pie",
+						radius: "55%",
+						center: ["50%", "60%"],
+						data: [
+							{ value: 635, name: "Chrome" },
+							{ value: 310, name: "Safari" },
+							{ value: 234, name: "Opera" },
+							{ value: 135, name: "Firefox" },
+							{ value: 148, name: "IE" }
+						],
+						itemStyle: {
+							emphasis: {
+								shadowBlur: 10,
+								shadowOffsetX: 0,
+								shadowColor: "rgba(0, 0, 0, 0.5)"
+							}
+						}
+					}
+				]
+			});
+		},
+		map() {}
 	}
 };
 </script>
@@ -72,11 +117,91 @@ export default {
 .admin_data {
 	padding: 30px;
 	box-sizing: border-box;
-	.PV_data {
-		max-width: 960px;
+	.card_list {
+		flex-wrap: wrap;
+		.card:last-of-type {
+			margin-bottom: 0px;
+		}
 	}
-	canvas {
-		// width: 100%;
+	.PV_data_wrap {
+		position: relative;
+		&::before {
+			content: "";
+			display: block;
+			padding-top: 52%;
+		}
+		.PV_data {
+			position: absolute;
+			left: 0px;
+			top: 0px;
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	.browser_data_wrap {
+		position: relative;
+		&::before {
+			content: "";
+			display: block;
+			padding-top: 100%;
+		}
+		.browser_data {
+			position: absolute;
+			left: 0px;
+			top: 0px;
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	.map_data_wrap {
+		position: relative;
+		width: 100%;
+		&::before {
+			content: "";
+			display: block;
+			padding-top: 40%;
+		}
+		.map_data {
+			position: absolute;
+			left: 0px;
+			top: 0px;
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	@media (max-width: 768px) {
+		.card_list {
+			flex-wrap: wrap;
+		}
+		.col7 {
+			width: 100%;
+		}
+		.col3 {
+			width: 100%;
+		}
+		.browser_data_wrap {
+			&::before {
+				padding-top: 60%;
+			}
+		}
+	}
+	@media (max-width: 540px) {
+		padding: 10px;
+		.PV_data_wrap {
+			&::before {
+				padding-top: 100%;
+			}
+		}
+	}
+	@media (max-width: 375px) {
+		.browser_data_wrap {
+			&::before {
+				padding-top: 100%;
+			}
+		}
 	}
 }
 </style>
