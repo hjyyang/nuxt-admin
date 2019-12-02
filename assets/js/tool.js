88b8c0fc8769f6c6a2aa3552db1744959e3b7eae
jon <jon@mmldigi.com>
2019-12-02 12:49:20 +0800

    feat: 添加滚动对应导航功能

--- assets/js/tool.js
+++ assets/js/tool.js
@@ -1,138 +1,159 @@
 var inProgress = null,
-	options = {};
+  options = {};
 
 function yTool(elem) {
-	return new yTool.prototype.init(elem);
+  return new yTool.prototype.init(elem);
 }
 yTool.prototype = {
-	constructor: yTool,
-	init: function(elem) {
-		this.elem = elem ? elem : null;
-	},
-	anchor: function(duration) {
-		options = {
-			start: window ? window.scrollY : 0,
-			prop: "scrollTo",
-			end: this.elem.offsetTop,
-			duration: duration,
-			startTime: Date.now(),
-			unit: "px"
-		};
-		inProgress = true;
-		animate();
-	},
-	run: function(prop, now) {
-		if (prop === "scrollTo") {
-			window.scrollTo(0, now);
-		}
-	}
+  constructor: yTool,
+  init: function (elem) {
+    this.elem = elem ? elem : null;
+  },
+  anchor: function (duration) {
+    options = {
+      start: window ? window.scrollY : 0,
+      prop: "scrollTo",
+      end: this.elem.offsetTop,
+      duration: duration,
+      startTime: Date.now(),
+      unit: "px"
+    };
+    inProgress = true;
+    animate();
+  },
+  run: function (prop, now) {
+    if (prop === "scrollTo") {
+      window.scrollTo(0, now);
+    }
+  }
 };
 yTool.prototype.init.prototype = yTool.prototype;
 
 function tick() {
-	//获取动画进度
-	var currentTime = Date.now(),
-		remaining = Math.max(
-			0,
-			options.startTime + options.duration - currentTime
-		),
-		temp = remaining / options.duration || 0,
-		percent = 1 - temp;
-	return percent;
+  //获取动画进度
+  var currentTime = Date.now(),
+    remaining = Math.max(
+      0,
+      options.startTime + options.duration - currentTime
+    ),
+    temp = remaining / options.duration || 0,
+    percent = 1 - temp;
+  return percent;
 }
 
 function animate() {
-	//持续执行动画函数
-	if (inProgress) {
-		if (document.hidden === false && window.requestAnimationFrame) {
-			window.requestAnimationFrame(animate);
-		}
-		let percent = tick(),
-			now = (options.end - options.start) * percent + options.start;
-		yTool.prototype.run(options.prop, now);
-		if (percent >= 1) {
-			inProgress = null;
-		}
-	}
+  //持续执行动画函数
+  if (inProgress) {
+    if (document.hidden === false && window.requestAnimationFrame) {
+      window.requestAnimationFrame(animate);
+    }
+    let percent = tick(),
+      now = (options.end - options.start) * percent + options.start;
+    yTool.prototype.run(options.prop, now);
+    if (percent >= 1) {
+      inProgress = null;
+    }
+  }
 }
 
 function lazy() {
-	//返回浏览器的可视区域位置
-	function getClient() {
-		var l, t, w, h;
-		l = document.documentElement.scrollLeft || document.body.scrollLeft;
-		t = document.documentElement.scrollTop || document.body.scrollTop;
-		w = document.documentElement.clientWidth;
-		h = document.documentElement.clientHeight;
-		return {
-			left: l,
-			top: t,
-			width: w,
-			height: h
-		};
-	}
-	function getItemRec(item) {
-		//返回待加载资源位置
-		var l = 0,
-			t = 0,
-			w,
-			h,
-			p;
-		w = item.offsetWidth;
-		h = item.offsetHeight;
-		l = item.offsetLeft;
-		t = item.offsetTop;
-		p = item.offsetParent;
-		while (p != null) {
-			l += p.offsetLeft;
-			t += p.offsetTop;
-			p = p.offsetParent;
-		}
-		return { left: l, top: t, width: w, height: h };
-	}
+  //返回浏览器的可视区域位置
+  function getClient() {
+    var l, t, w, h;
+    l = document.documentElement.scrollLeft || document.body.scrollLeft;
+    t = document.documentElement.scrollTop || document.body.scrollTop;
+    w = document.documentElement.clientWidth;
+    h = document.documentElement.clientHeight;
+    return {
+      left: l,
+      top: t,
+      width: w,
+      height: h
+    };
+  }
 
-	function cross(rect1, rect2) {
-		//判断两个矩形是否相交,返回一个布尔值
-		var lc1, lc2, tc1, tc2, w1, h1;
-		lc1 = rect1.left + rect1.width / 2;
-		lc2 = rect2.left + rect2.width / 2;
-		tc1 = rect1.top + rect1.height / 2;
-		tc2 = rect2.top + rect2.height / 2;
-		w1 = (rect1.width + rect2.width) / 2;
-		h1 = (rect1.height + rect2.height) / 2;
-		return Math.abs(lc1 - lc2) < w1 && Math.abs(tc1 - tc2) < h1;
-	}
+  function getItemRec(item) {
+    //返回待加载资源位置
+    var l = 0,
+      t = 0,
+      w,
+      h,
+      p;
+    w = item.offsetWidth;
+    h = item.offsetHeight;
+    l = item.offsetLeft;
+    t = item.offsetTop;
+    p = item.offsetParent;
+    while (p != null) {
+      l += p.offsetLeft;
+      t += p.offsetTop;
+      p = p.offsetParent;
+    }
+    return {
+      left: l,
+      top: t,
+      width: w,
+      height: h
+    };
+  }
 
-	function currentShow(arr, rect1, callback) {
-		//比较某个目标区域是否呈现在浏览器区域
-		let rect2;
-		for (let i = arr.length - 1; i >= 0; i--) {
-			if (arr[i]) {
-				rect2 = getItemRec(arr[i]);
-				if (cross(rect1, rect2)) {
-					callback(arr[i]);
-					//加载资源后，删除监测
-					arr.splice(i, 1);
-				}
-			}
-		}
-	}
+  function cross(rect1, rect2) {
+    //判断两个矩形是否相交,返回一个布尔值
+    var lc1, lc2, tc1, tc2, w1, h1;
+    lc1 = rect1.left + rect1.width / 2;
+    lc2 = rect2.left + rect2.width / 2;
+    tc1 = rect1.top + rect1.height / 2;
+    tc2 = rect2.top + rect2.height / 2;
+    w1 = (rect1.width + rect2.width) / 2;
+    h1 = (rect1.height + rect2.height) / 2;
+    return Math.abs(lc1 - lc2) < w1 && Math.abs(tc1 - tc2) < h1;
+  }
 
-	function autocheck(arr) {
-		//检测目标对象是否出现在客户区
-		var rect1 = getClient();
-		currentShow(arr, rect1, function(obj) {
-			//加载资源...
-			obj.src = obj.getAttribute("_src");
-		});
-	}
-	var lazyImageArr = [];
-	document.querySelectorAll("img.lazysize").forEach(dom => {
-		lazyImageArr.push(dom);
-	});
-	window.addEventListener("scroll", function() {
-		autocheck(lazyImageArr);
-	});
+  function currentShow(arr, rect1, callback) {
+    //比较某个目标区域是否呈现在浏览器区域
+    let rect2;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i]) {
+        rect2 = getItemRec(arr[i]);
+        if (cross(rect1, rect2)) {
+          callback(arr[i]);
+        }
+      }
+    }
+  }
+
+  function autocheck(arr) {
+    //检测目标对象是否出现在客户区
+    var rect1 = getClient();
+    currentShow(arr, rect1, function (obj) {
+      //加载资源...
+      if (obj.getAttribute("data-src")) {
+        obj.src = obj.getAttribute("data-src");
+        //加载资源后，删除监测
+        obj.classList.remove('lazyload');
+        obj.classList.add('lazyloaded');
+      }
+      if (obj.classList.contains('on')) {
+        document.querySelectorAll(".corresponding").forEach(item => {
+          item.classList.remove('on');
+        })
+        document.querySelectorAll(".corresponding")[+obj.getAttribute('data-index')].classList.add('on');
+      }
+    });
+  }
+  var lazyArr = document.querySelectorAll(".lazyload");
+  autocheck(lazyArr);
+
+  window.addEventListener("scroll", function () {
+    lazyArr = document.querySelectorAll(".lazyload");
+    if (lazyArr.length <= 0) return false;
+    autocheck(lazyArr);
+  });
+  window.addEventListener("resize", function () {
+    lazyArr = document.querySelectorAll(".lazyload");
+    if (lazyArr.length <= 0) return false;
+    autocheck(lazyArr);
+  });
 }
 
 yTool.lazysize = yTool.prototype.lazy = lazy;
--- pages/music/index.vue
+++ pages/music/index.vue
@@ -1,94 +1,244 @@
 <template>
-	<main class="music">
-		<section class="page_banner">
-			<div class="minContainer">
-				<h1>音乐，响彻心灵</h1>
-			</div>
-		</section>
-		<section class="music_wrap">
-			<div class="fiexd_nav">
-				<div class="nav_wrap">
-					<ul @click="selectCategory">
-						<li class="current">纯音乐</li>
-						<li>民谣</li>
-						<li>吉他曲</li>
-					</ul>
-				</div>
-			</div>
-			<div class="minContainer">
-				<div class="content">
-					<div class="item">
-						<h2 class="col_title">
-							<div class="title_wrap">
-								<div class="title_main">
-									<span>纯音乐</span>
-									<div class="play">
-										<i class="el-icon-video-play"></i>顺序播放
-									</div>
-								</div>
-							</div>
-						</h2>
-						<ul>
-							<li>
-								<nuxt-link to="/music/天空之城">
-									<div class="poster">
-										<img _src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazysize" alt />
-									</div>
-									<div class="title">天空之城</div>
-								</nuxt-link>
-							</li>
-							<li>
-								<nuxt-link to>
-									<div class="poster">
-										<img _src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazysize" alt />
-									</div>
-									<div class="title">无题</div>
-								</nuxt-link>
-							</li>
-							<li>
-								<nuxt-link to>
-									<div class="poster">
-										<img _src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazysize" alt />
-									</div>
-									<div class="title">天空之城</div>
-								</nuxt-link>
-							</li>
-							<!-- <li>
-								<nuxt-link to>
-									<div class="poster">
-										<img src="/upload/2019/10/tiankongzhichneg.jpeg" alt />
-									</div>
-									<div class="title">天空之城</div>
-								</nuxt-link>
-							</li>
-							<li>
-								<nuxt-link to>
-									<div class="poster">
-										<img src="/upload/2019/10/tiankongzhichneg.jpeg" alt />
-									</div>
-									<div class="title">天空之城</div>
-								</nuxt-link>
-							</li> -->
-						</ul>
-					</div>
-				</div>
-			</div>
-		</section>
-	</main>
+    <main class="music">
+        <section class="page_banner">
+            <div class="minContainer">
+                <h1>音乐，响彻心灵</h1>
+            </div>
+        </section>
+        <section class="music_wrap">
+            <div class="fiexd_nav">
+                <div class="nav_wrap">
+                    <ul @click="selectCategory">
+                        <li class="on corresponding">纯音乐</li>
+                        <li class="corresponding">民谣</li>
+                        <li class="corresponding">吉他曲</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="minContainer">
+                <div class="content">
+                    <div class="item lazyload on" data-index="0">
+                        <h2 class="col_title">
+                            <div class="title_wrap">
+                                <div class="title_main">
+                                    <span>纯音乐</span>
+                                    <div class="play">
+                                        <i class="el-icon-video-play"></i>顺序播放
+                                    </div>
+                                </div>
+                            </div>
+                        </h2>
+                        <ul>
+                            <li>
+                                <nuxt-link to="/music/天空之城">
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">天空之城</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">无题</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">天空之城</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">无题</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">天空之城</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">无题</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">天空之城</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">无题</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">天空之城</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">无题</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">天空之城</div>
+                                </nuxt-link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="item lazyload on" data-index="1">
+                        <h2 class="col_title">
+                            <div class="title_wrap">
+                                <div class="title_main">
+                                    <span>民谣</span>
+                                    <div class="play">
+                                        <i class="el-icon-video-play"></i>顺序播放
+                                    </div>
+                                </div>
+                            </div>
+                        </h2>
+                        <ul>
+                            <li>
+                                <nuxt-link to="/music/天空之城">
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">天空之城</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">无题</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">天空之城</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">无题</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">天空之城</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">无题</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">天空之城</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">无题</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">天空之城</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">无题</div>
+                                </nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to>
+                                    <div class="poster">
+                                        <img data-src="/upload/2019/10/tiankongzhichneg.jpeg" class="lazyload" alt />
+                                    </div>
+                                    <div class="title">天空之城</div>
+                                </nuxt-link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 </template>
 
 <script>
 import yTool from "~/assets/js/tool";
 export default {
-	data() {
-		return {};
-	},
-	asyncData() {},
-	mounted() {
-		yTool.lazysize();
-	},
-	methods: {
-		selectCategory: function() {}
-	}
+    data() {
+        return {};
+    },
+    asyncData() {},
+    mounted() {
+        yTool.lazysize();
+    },
+    methods: {
+        selectCategory: function() {}
+    }
 };
 </script>
