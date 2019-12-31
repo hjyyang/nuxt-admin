09632b59b594b29627e818d7a2e1b7f04dcda2e4
jon <jon@mmldigi.com>
2019-12-31 18:05:35 +0800

    feat: 添加用户新增接口

--- server/api/user.js
+++ server/api/user.js
@@ -4,70 +4,92 @@ const md5 = require("md5-node");
 const validator = require("validator");
 
 const router = new Router({
-	prefix: "/api"
+  prefix: "/api"
 });
 
 let limit = 10;
 router.post("/findAllUser", async ctx => {
-	let page = ctx.request.body.page ? ctx.request.body.page : "",
-		pageStr = page + "";
-	if (validator.isInt(pageStr) && !validator.isEmpty(pageStr)) {
-		let offset = page - 1;
-		var UserTable = await User.findAndCountAll({
-			//查询并计数所有
-			offset: offset * limit,
-			limit: limit
-		});
+  let page = ctx.request.body.page;
+  page = page ? page : "";
+  if (validator.isInt(page + "") && !validator.isEmpty(page + "")) {
+    let offset = page - 1;
+    var UserTable = await User.findAndCountAll({
+      //查询并计数所有
+      offset: offset * limit,
+      limit: limit
+    });
 
-		ctx.body = {
-			count: UserTable.count,
-			users: UserTable.rows
-		};
-	} else {
-		ctx.body = {
-			status: 10000,
-			massage: "请输入正确字段或值！"
-		};
-	}
+    ctx.body = {
+      count: UserTable.count,
+      users: UserTable.rows
+    };
+  } else {
+    ctx.body = {
+      status: 10000,
+      massage: "请输入正确字段或值！"
+    };
+  }
 });
 
 router.post("/updateUser", async ctx => {
-	let id = ctx.request.body.user_id,
-		role = ctx.request.body.role;
-	ctx.body = {
-		result: true
-	};
+  let id = ctx.request.body.user_id,
+    role = ctx.request.body.role;
+  ctx.body = {
+    result: true
+  };
 });
 
 router.post("/deteleUser", async ctx => {
-	let id = ctx.request.body.user_id;
+  let id = ctx.request.body.user_id;
 
-	ctx.body = {
-		result: true
-	};
+  ctx.body = {
+    result: true
+  };
 });
 
 router.post("/addUser", async ctx => {
-	let name = ctx.request.body.user_name,
-		password = ctx.request.body.password,
-		email = ctx.request.body.email;
+  let name = ctx.request.body.user_name ? ctx.request.body.user_name : '',
+    password = ctx.request.body.password ? ctx.request.body.password : '',
+    email = ctx.request.body.email ? ctx.request.body.email : '';
 
-	User.create({
-		name: name,
-		password: password,
-		email: email
-	}).then(res => {
-		// console.log('123,',res);
-	});
-
-	ctx.body = {
-		result: true
-	};
+  if (validator.isEmpty(name + '') || validator.isEmpty(password + '')) {
+    return ctx.body = {
+      status: 10000,
+      message: '用户名或密码不能为空！'
+    };
+  } else {
+    if (email && !validator.isEmail(email + '')) {
+      return ctx.body = {
+        status: 10000,
+        message: '邮箱格式不正确！'
+      };
+    }
+  }
+  let result = await User.findOrCreate({
+    where: {
+      name: name
+    },
+    defaults: {
+      password: md5(password),
+      email: email
+    }
+  });
+  if (result[1]) {
+    return ctx.body = {
+      status: 8888,
+      message: '创建成功！'
+    }
+  } else {
+    return ctx.body = {
+      status: 10001,
+      message: '用户名已存在！'
+    }
+  }
 });
 
 router.post("/findUser", async ctx => {
-	ctx.body = {
-		result: true
-	};
+  ctx.body = {
+    result: true
+  };
 });
 module.exports = router;
