8591af2ad157961e83cb734018b42af5756cbf1b
jon <13560586496@163.com>
2021-01-12 20:57:10 +0800

    fact: 添加备案号链接

--- components/footer.vue
+++ components/footer.vue
@@ -1,19 +1,22 @@
 <template>
-    <footer>Copyright © 2019 风间的少年 All Rights Reserved.<br/> 备案号：粤ICP备18106900号-1</footer>
+	<footer>
+		Copyright © 2019 风间的少年 All Rights Reserved.<br />
+		备案号：<a href="https://beian.miit.gov.cn" target="_blank">粤ICP备18106900号-1</a>
+	</footer>
 </template>
 
 <script>
 export default {
-    data() {
-        return {};
-    },
-    asyncData() {}
+	data() {
+		return {};
+	},
+	asyncData() {},
 };
 </script>
 
 <style lang="scss">
 footer {
-    min-height: 60px;
-    text-align: center;
+	min-height: 60px;
+	text-align: center;
 }
 </style>
